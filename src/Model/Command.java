0a57fb8210ce9665a1220f374b044b600b70cb35
bikethief <marlonschlosshauer@gmail.com>
2019-03-31 01:51:03 +0100

    Variablen mit Setter und Getter

--- src/Model/Command.java
+++ src/Model/Command.java
@@ -1,14 +1,46 @@
 package Model;
 
-import Model.Instruction;
-
 public class Command {
-    Instruction instruction;
-    int value[];
+    private Instruction instruction;
+    private int value[];
+
+    private boolean next;
+    private boolean selected;
 
     public Command(Instruction instruction, int value[]) {
         this.instruction = instruction;
         this.value = value;
     }
 
+    public Instruction getInstruction() {
+        return instruction;
+    }
+
+    public void setInstruction(Instruction instruction) {
+        this.instruction = instruction;
+    }
+
+    public int[] getValue() {
+        return value;
+    }
+
+    public void setValue(int[] value) {
+        this.value = value;
+    }
+
+    public boolean isNext() {
+        return next;
+    }
+
+    public void setNext(boolean next) {
+        this.next = next;
+    }
+
+    public boolean isSelected() {
+        return selected;
+    }
+
+    public void setSelected(boolean selected) {
+        this.selected = selected;
+    }
 }
