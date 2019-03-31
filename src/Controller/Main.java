76bd5f8c31f872e42074dc2befa3603325aff069
bikethief <marlonschlosshauer@gmail.com>
2019-03-31 01:02:25 +0100

    Eclipse, IntelliJ, Windows und macOS Dateien exkludiert.

--- src/Controller/Main.java
+++ src/Controller/Main.java
@@ -4,9 +4,9 @@ public class Main {
 
     public static void main(String args[]) {
         Parser parser = new Parser();
-        //ArrayList<String> commands = parser.load("/Users/akira/Downloads/LST/LST1.LST");
-        ArrayList<String> commands = parser.load("H:\\2.Semester\\Rechnerarchitektur\\SimTest_OG\\TPicSim1.LST");
-        
+
+        ArrayList<String> commands = parser.load("./tests/raw/TPicSim1.LST");
+
         ArrayList<String> commandos = parser.parse(commands);
         for (int i = 0; i<commandos.size(); i++) {
         	System.out.println(commandos.get(i));
