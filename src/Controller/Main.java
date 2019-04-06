af31ab8fc253f95f1d58acc3acb5819073a5720a
marlonschlosshauer <marlonschlosshauer@gmail.com>
2019-04-06 17:59:34 +0200

    Formatierung angepasst

--- src/Controller/Main.java
+++ src/Controller/Main.java
@@ -5,40 +5,21 @@ import Model.Worker;
 import View.Primary;
 
 import javax.swing.*;
-import java.util.ArrayList;
 import java.util.Observable;
 import java.util.Observer;
 
 public class Main implements Observer {
-
-    public static void main(String args[]) {
-
-
-        //TODO: Interface zwischen Primary und Model ausarbeiten
-
-        /*
-
-        Parser parser = new Parser();
-        ArrayList<String> commands = parser.cut(parser.load("./src/tests/raw/TPicSim1.LST"));
-
-        for (int i = 0; i < commands.size(); i++) {
-            System.out.println(commands.get(i));
-        }
-        */
-        Main manager = new Main();
-        manager.start();
-    }
-
-
     private Worker peon;
     private Primary view;
 
-    public void start(){
+
+    public void start() {
 
         peon = new Worker();
         view = new Primary();
         view.initialize();
         this.initialiseActionListeners(peon, view);
+        peon.addObserver(this);
 
         // Mache GUI sichtbar
         view.start();
@@ -46,11 +27,21 @@ public class Main implements Observer {
 
     public void initialiseActionListeners(Worker peon, Primary view) {
         JButton buttons[] = view.getButtons();
-        buttons[0].addActionListener( e -> {this.run(peon);});
-        buttons[1].addActionListener( e -> {this.stop(peon);});
-        buttons[2].addActionListener( e -> {this.step(peon);});
-        buttons[3].addActionListener( e -> {this.reset(peon);});
-        buttons[4].addActionListener( e -> {this.load(peon,view);});
+        buttons[0].addActionListener(e -> {
+            this.run(peon);
+        });
+        buttons[1].addActionListener(e -> {
+            this.stop(peon);
+        });
+        buttons[2].addActionListener(e -> {
+            this.step(peon);
+        });
+        buttons[3].addActionListener(e -> {
+            this.reset(peon);
+        });
+        buttons[4].addActionListener(e -> {
+            this.load(peon, view);
+        });
     }
 
     public void run(Worker peon) {
@@ -66,14 +57,16 @@ public class Main implements Observer {
     }
 
     public void load(Worker peon, Primary view) {
-        //System.out.println(view.invokeFileChooser());
-        peon.feed(Parser.load(view.invokeFileChooser()));
+        peon.feed(Parser.parse(Parser.cut(Parser.load(view.invokeFileChooser()))));
 
         view.getList().setModel(new OperationModel(peon.getCounter()));
         //TODO: view.getList().addListSelectionListener(new SelectionListener());
     }
 
-    public void update() {
+    @Override
+    public void update(Observable o, Object arg) {
+        // TODO: Nachricht von Worker ? Dependency unschoen!
+
         JLabel registers[] = this.view.getRegisters();
 
         registers[0].setText(this.peon.getMemory()[0] + "");
@@ -89,11 +82,10 @@ public class Main implements Observer {
         registers[11].setText(this.peon.getWorking() + "");
     }
 
-    @Override
-    public void update(Observable o, Object arg) {
-        //TODO: Pruefen ob Nachricht wirklich von Worker kommt.
-        //TODO: Unit Test bauen
-        this.update();
+    public static void main(String args[]) {
+        Main manager = new Main();
+        manager.start();
     }
+
 }
 
