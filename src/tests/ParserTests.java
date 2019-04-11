cfe0043d62e6b2c1d0ad5965c8fc17f3bff478a8
marlonschlosshauer <marlonschlosshauer@gmail.com>
2019-04-11 10:54:27 +0200

    Test um Zufallszahl auszulesen hinzugefuegt

--- src/tests/ParserTests.java
+++ src/tests/ParserTests.java
@@ -23,4 +23,30 @@ class ParserTests {
         // Check ob resultat korrekt ist
         assertEquals(result, new Command(Instruction.MOVLW, new int[]{0x11}));
     }
+
+    @Test
+    void convertRandomTest() {
+        // Convertiere Hex codes in Command mit korrektem Command und Wert(en)
+
+        // Erstelle benoetigte variablen
+        String raw = "0000 30";
+
+        // Zufallszahl generieren
+        int value = (int) (100 * Math.random());
+
+
+        // Zahl in Text einfuegen
+        if(value < 16) {
+            raw += '0';
+        }
+
+        raw += Integer.toHexString(value);
+        raw += "           00017           movlw XXh           ;in W steht nun 11h, Statusreg. unveraendert";
+
+        // Convertiere von String zu Command
+        Command result = Parser.parse(Parser.cut(raw));
+
+        // Check ob resultat korrekt ist
+        assertEquals(result, new Command(Instruction.MOVLW, new int[]{value}));
+    }
 }
